609123d479b51cf30588be591d59a68a00ad62b0
Martin Andrews <GitHub@mdda.net>
2014-07-31 03:34:28 +0800

    Perceptron first for implementation

--- src/main/scala/ConciseGreedyDependencyParser.scala
+++ src/main/scala/ConciseGreedyDependencyParser.scala
@@ -8,7 +8,79 @@ package object ConciseGreedyDependencyParser {
     def apply(idx: Int): String = if(idx>=0 || idx<list.length) list(idx) else default
   }
 
+
 /*
+class Perceptron(object):
+    def __init__(self, classes=None):
+    
+        # Each feature gets its own weight vector, so weights is a dict-of-arrays
+        self.classes = classes
+        self.weights = {}
+        
+        # The accumulated values, for the averaging. These will be keyed by feature/class tuples
+        self._totals = defaultdict(int)
+        
+        # The last time the feature was changed, for the averaging. Also keyed by feature/class tuples
+        # (tstamps is short for timestamps)
+        self._tstamps = defaultdict(int)
+        
+        # Number of instances seen
+        self.i = 0
+
+    def predict(self, features):
+        '''Dot-product the features and current weights and return the best class.'''
+        scores = self.score(features)
+        
+        # Do a secondary alphabetic sort, for stability
+        return max(self.classes, key=lambda clas: (scores[clas], clas))
+
+    def score(self, features):
+        all_weights = self.weights
+        scores = dict((clas, 0) for clas in self.classes)
+        for feat, value in features.items():
+            if value == 0:
+                continue
+            if feat not in all_weights:
+                continue
+            weights = all_weights[feat]
+            for clas, weight in weights.items():
+                scores[clas] += value * weight
+        return scores
+
+    def update(self, truth, guess, features):       
+        def upd_feat(c, f, w, v):
+            param = (f, c)
+            self._totals[param] += (self.i - self._tstamps[param]) * w
+            self._tstamps[param] = self.i
+            self.weights[f][c] = w + v
+
+        self.i += 1
+        if truth == guess:
+            return None
+        for f in features:
+            weights = self.weights.setdefault(f, {})
+            upd_feat(truth, f, weights.get(truth, 0.0), 1.0)
+            upd_feat(guess, f, weights.get(guess, 0.0), -1.0)
+
+    def average_weights(self):
+        for feat, weights in self.weights.items():
+            new_feat_weights = {}
+            for clas, weight in weights.items():
+                param = (feat, clas)
+                total = self._totals[param]
+                total += (self.i - self._tstamps[param]) * weight
+                averaged = round(total / float(self.i), 3)
+                if averaged:
+                    new_feat_weights[clas] = averaged
+            self.weights[feat] = new_feat_weights
+
+    def save(self, path):
+        print "Saving model to %s" % path
+        pickle.dump(self.weights, open(path, 'w'))
+
+    def load(self, path):
+        self.weights = pickle.load(open(path))
+
 
 def train(parser, sentences, nr_iter):
     parser.tagger.start_training(sentences)
