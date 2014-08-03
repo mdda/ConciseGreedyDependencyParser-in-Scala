27455a41ac79b96597f90c5f11f0a5a4f756925f
Martin Andrews <GitHub@mdda.net>
2014-08-04 02:33:49 +0800

    Perceptron needn't know ClassNames, only ClassNums

--- src/main/scala/ConciseGreedyDependencyParser.scala
+++ src/main/scala/ConciseGreedyDependencyParser.scala
@@ -16,9 +16,12 @@ import java.io.{PrintWriter, File}
 import scala.io.{Source}
 
 package object ConciseGreedyDependencyParserObj {
+  type ClassNum  = Int
   type ClassName = String
   type FeatureName = String
   type FeatureData = String
+  type Word = String
+  type Sentence = Vector[Word]
 }
 
 import ConciseGreedyDependencyParserObj._
@@ -29,9 +32,8 @@ case class DefaultList(list:List[String], default:String="") {
   def apply(idx: Int): String = if(idx>=0 || idx<list.length) list(idx) else default
 }
 
-class Perceptron(classes:Vector[ClassName]) {
+class Perceptron(n_classes:Int) {
   // These need not be visible outside the class
-  type ClassNum  = Int
   type TimeStamp = Int
   
   case class WeightLearner(current:Int, total:Int, ts:TimeStamp) {
@@ -58,11 +60,8 @@ class Perceptron(classes:Vector[ClassName]) {
   type Score = Float
   type ClassVector = Vector[Score]
 
-  def getClassNum(class_name: ClassName): ClassNum = classes.indexOf(class_name) // -1 => "CLASS-NOT-FOUND"
-
-  def predict(classnum_vector : ClassVector) : ClassName = { // Return best class guess for this vector of weights
-    val best_classnum = classnum_vector.zipWithIndex.maxBy(_._1)._2   // in vector order (stabilizes) ///NOT : (and alphabetically too)
-    classes(best_classnum)
+  def predict(classnum_vector : ClassVector) : ClassNum = { // Return best class guess for this vector of weights
+    classnum_vector.zipWithIndex.maxBy(_._1)._2   // in vector order (stabilizes) ///NOT : (and alphabetically too)
   }
 
   def average(w : WeightLearner):Float = (w.current*(seen-w.ts) + w.total) / seen // This is dynamically calculated
@@ -71,7 +70,7 @@ class Perceptron(classes:Vector[ClassName]) {
   def score(features: Map[Feature, Score], score_method: WeightLearner => Float): ClassVector = { // Return 'dot-product' score for all classes
     features
       .filter( pair => pair._2 != 0 )  // if the 'score' multiplier is zero, skip
-      .foldLeft( Vector.fill(classes.length)(0:Float) ){ case (acc, (Feature(name,data), score)) => {  // Start with a zero classnum->score vector
+      .foldLeft( Vector.fill(n_classes)(0:Float) ){ case (acc, (Feature(name,data), score)) => {  // Start with a zero classnum->score vector
         learning
           .getOrElse(name, Map.empty[String,ClassToWeightLearner])   // This is first level of feature access
             .getOrElse(data, Map.empty[ ClassNum,  WeightLearner ])       // This is second level of feature access and is a Map of ClassNums to Weights (or NOOP if not there)
@@ -83,16 +82,15 @@ class Perceptron(classes:Vector[ClassName]) {
       }}
   }
   
-  def update(truth:ClassName, guess:ClassName, features:List[Feature]): Unit = { // Hmmm ..Unit..
+  def update(truth:ClassNum, guess:ClassNum, features:List[Feature]): Unit = { // Hmmm ..Unit..
     seen += 1
     if(truth != guess) {
       for {
         feature <- features
       } {
         val cn_wl = learning.getOrElse(feature.name, Map.empty[String,ClassToWeightLearner]).getOrElse(feature.data, mutable.Map.empty[ ClassNum,  WeightLearner ])
-        val (n_guess, n_truth) = (getClassNum(guess), getClassNum(truth))
-        cn_wl.update(n_guess, cn_wl.getOrElse(n_guess, WeightLearnerInitial ).update(-1))  // This REALLY OUGHT to exist, since it is what I chose (incorrectly)...
-        cn_wl.update(n_truth, cn_wl.getOrElse(n_truth, WeightLearnerInitial ).update(+1))  // This could easily be a new entry
+        cn_wl.update(guess, cn_wl.getOrElse(guess, WeightLearnerInitial ).update(-1))  // This REALLY OUGHT to exist, since it is what I chose (incorrectly)...
+        cn_wl.update(truth, cn_wl.getOrElse(truth, WeightLearnerInitial ).update(+1))  // This could easily be a new entry
       }
     }
   }
@@ -131,89 +129,171 @@ class Perceptron(classes:Vector[ClassName]) {
     learning ++= unpickled
   }
 
-/*  
-    def save(self, path):
-        print "Saving model to %s" % path
-        pickle.dump(self.weights, open(path, 'w'))
-        
-    def load(self, path):
-        self.weights = pickle.load(open(path))
-*/  
 }
 
+class Tagger(path:String, classes:Vector[ClassName], tag_dict:Map[Word, ClassNum]) {
+  def getClassNum(class_name: ClassName): ClassNum = classes.indexOf(class_name) // -1 => "CLASS-NOT-FOUND"
 
-/* class Perceptron(object):
-    def __init__(self, classes=None):
+  //val perceptron = new Perceptron(classes)
+}
+
+object Tagger {
+  // Make a tag dictionary for single-tag words : So that they can be 'resolved' immediately
+  // Also builds the class list
+  def _make_tagdict(training_sentences: List[Sentence]) = {
     
-        # Each feature gets its own weight vector, so weights is a dict-of-arrays
-        self.classes = classes
-        self.weights = {}
-        
-        # The accumulated values, for the averaging. These will be keyed by feature/class tuples
-        self._totals = defaultdict(int)
-        
-        # The last time the feature was changed, for the averaging. Also keyed by feature/class tuples
-        # (tstamps is short for timestamps)
-        self._tstamps = defaultdict(int)
-        
-        # Number of instances seen
-        self.i = 0
+    
+  }
+}
 
-    def predict(self, features):
-        '''Dot-product the features and current weights and return the best class.'''
-        scores = self.score(features)
-        
-        # Do a secondary alphabetic sort, for stability
-        return max(self.classes, key=lambda clas: (scores[clas], clas))
-
-    def score(self, features):
-        all_weights = self.weights
-        scores = dict((clas, 0) for clas in self.classes)
-        for feat, value in features.items():
-            if value == 0:
-                continue
-            if feat not in all_weights:
-                continue
-            weights = all_weights[feat]
-            for clas, weight in weights.items():
-                scores[clas] += value * weight
-        return scores
-*/
 /*
-    def update(self, truth, guess, features):       
-        def upd_feat(c, f, w, v):
-            param = (f, c)
-            self._totals[param] += (self.i - self._tstamps[param]) * w
-            self._tstamps[param] = self.i
-            self.weights[f][c] = w + v
+    def _make_tagdict(self, sentences):
+        counts = defaultdict(lambda: defaultdict(int))
+        for sent in sentences:
+            #print type(sent[1])
+            for word, tag in zip(sent[0], sent[1]):
+                #print " %s : %s" % (word, tag, )
+                counts[word][tag] += 1
+                self.classes.add(tag)
+        freq_thresh = 20
+        ambiguity_thresh = 0.97
+        for word, tag_freqs in counts.items():
+            tag, mode = max(tag_freqs.items(), key=lambda item: item[1])
+            n = sum(tag_freqs.values())
+            # Don't add rare words to the tag dictionary
+            # Only add quite unambiguous words
+            if n >= freq_thresh and (float(mode) / n) >= ambiguity_thresh:
+                self.tagdict[word] = tag
+                print "_make_tagdict added : %10s -> %10s" % (word, tag, )
 
-        self.i += 1
-        if truth == guess:
-            return None
-        for f in features:
-            weights = self.weights.setdefault(f, {})
-            upd_feat(truth, f, weights.get(truth, 0.0), 1.0)
-            upd_feat(guess, f, weights.get(guess, 0.0), -1.0)
+*/  
 
-    def average_weights(self):
-        for feat, weights in self.weights.items():
-            new_feat_weights = {}
-            for clas, weight in weights.items():
-                param = (feat, clas)
-                total = self._totals[param]
-                total += (self.i - self._tstamps[param]) * weight
-                averaged = round(total / float(self.i), 3)
-                if averaged:
-                    new_feat_weights[clas] = averaged
-            self.weights[feat] = new_feat_weights
 
-    def save(self, path):
-        print "Saving model to %s" % path
-        pickle.dump(self.weights, open(path, 'w'))
+/* 
+class PerceptronTagger(object):
+    '''Greedy Averaged Perceptron tagger'''
+    model_loc = os.path.join(os.path.dirname(__file__), 'models/tagger.pickle')
+    def __init__(self, classes=None, load=True):
+        self.tagdict = {}
+        if classes:
+            self.classes = classes
+        else:
+            self.classes = set()
+        self.model = Perceptron(self.classes)
+        if load:
+            self.load(PerceptronTagger.model_loc)
 
-    def load(self, path):
-        self.weights = pickle.load(open(path))
+    // This builds a list of tags corresponding to the predictions of the Dictionary and the Perceptron
+    def tag(self, words, tokenize=True):
+        prev, prev2 = START
+        tags = DefaultList('') 
+        context = START + [self._normalize(w) for w in words] + END
+        for i, word in enumerate(words):
+            tag = self.tagdict.get(word)
+            if not tag:
+                features = self._get_features(i, word, context, prev, prev2)
+                tag = self.model.predict(features)
+            tags.append(tag)
+            prev2 = prev; prev = tag
+        return tags
 
+    // This creates the tagdict initially from the sentences (first thing to occur)
+    // Also builds the classlist
+    def start_training(self, sentences):
+        self._make_tagdict(sentences)
+        self.model = Perceptron(self.classes)
+
+    """  UNUSED : THERE IS NO end_training here...
+    def train(self, sentences, save_loc=None, nr_iter=5):
+        '''Train a model from sentences, and save it at save_loc. nr_iter
+        controls the number of Perceptron training iterations.'''
+        self.start_training(sentences)
+        for iter_ in range(nr_iter):
+            for words, tags in sentences:
+                self.train_one(words, tags)
+            random.shuffle(sentences)
+        self.end_training(save_loc)
+    """
+    
+    def save(self):
+        # Pickle as a binary file
+        pickle.dump((self.model.weights, self.tagdict, self.classes),
+                    open(PerceptronTagger.model_loc, 'wb'), -1)
+
+    def train_one(self, words, tags):
+        prev, prev2 = START
+        context = START + [self._normalize(w) for w in words] + END
+        for i, word in enumerate(words):
+            guess = self.tagdict.get(word)
+            if not guess:
+                feats = self._get_features(i, word, context, prev, prev2)
+                guess = self.model.predict(feats)
+                self.model.update(tags[i], guess, feats)
+            prev2 = prev; prev = guess
+
+    def load(self, loc):
+        w_td_c = pickle.load(open(loc, 'rb'))
+        self.model.weights, self.tagdict, self.classes = w_td_c
+        self.model.classes = self.classes
+
+    def _normalize(self, word):
+        if '-' in word and word[0] != '-':
+            return '!HYPHEN'
+        elif word.isdigit() and len(word) == 4:
+            return '!YEAR'
+        elif word[0].isdigit():
+            return '!DIGITS'
+        else:
+            return word.lower()
+
+    def _get_features(self, i, word, context, prev, prev2):
+        '''Map tokens into a feature representation, implemented as a
+        {hashable: float} dict. If the features change, a new model must be
+        trained.'''
+        def add(name, *args):
+            features[' '.join((name,) + tuple(args))] += 1
+
+        i += len(START)
+        features = defaultdict(int)
+        # It's useful to have a constant feature, which acts sort of like a prior
+        add('bias')
+        add('i suffix', word[-3:])
+        add('i pref1', word[0])
+        add('i-1 tag', prev)
+        add('i-2 tag', prev2)
+        add('i tag+i-2 tag', prev, prev2)
+        add('i word', context[i])
+        add('i-1 tag+i word', prev, context[i])
+        add('i-1 word', context[i-1])
+        add('i-1 suffix', context[i-1][-3:])
+        add('i-2 word', context[i-2])
+        add('i+1 word', context[i+1])
+        add('i+1 suffix', context[i+1][-3:])
+        add('i+2 word', context[i+2])
+        return features
+
+    def _make_tagdict(self, sentences):
+        '''Make a tag dictionary for single-tag words.'''
+        counts = defaultdict(lambda: defaultdict(int))
+        for sent in sentences:
+            #print type(sent[1])
+            for word, tag in zip(sent[0], sent[1]):
+                #print " %s : %s" % (word, tag, )
+                counts[word][tag] += 1
+                self.classes.add(tag)
+        freq_thresh = 20
+        ambiguity_thresh = 0.97
+        for word, tag_freqs in counts.items():
+            tag, mode = max(tag_freqs.items(), key=lambda item: item[1])
+            n = sum(tag_freqs.values())
+            # Don't add rare words to the tag dictionary
+            # Only add quite unambiguous words
+            if n >= freq_thresh and (float(mode) / n) >= ambiguity_thresh:
+                self.tagdict[word] = tag
+                print "_make_tagdict added : %10s -> %10s" % (word, tag, )
+
+def _pc(n, d):
+    return (float(n) / d) * 100
 
 def train(parser, sentences, nr_iter):
     parser.tagger.start_training(sentences)
