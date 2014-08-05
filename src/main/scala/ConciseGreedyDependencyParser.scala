e8709bb2d43a80f18718e5c07102b52a39a28132
Martin Andrews <GitHub@mdda.net>
2014-08-06 01:51:03 +0800

    Nicer functional switch in tag_dict

--- src/main/scala/ConciseGreedyDependencyParser.scala
+++ src/main/scala/ConciseGreedyDependencyParser.scala
@@ -61,10 +61,8 @@ class Perceptron(n_classes:Int) {
       WeightLearner(current + change, total + current*(seen-ts), seen)
     }
   }
-  //def WeightLearnerInitial(s:TimeStamp) = WeightLearner(0, 0, s)
   
   type ClassToWeightLearner = mutable.Map[ ClassNum,  WeightLearner ]  // tells us the stats of each class (if present) 
-  //type ClassToWeightLearner = WithDefault[mutable.Map[ ClassNum,  WeightLearner ], f=>WeightLearner() ]  // tells us the stats of each class (if present) 
   
   // The following are keyed on feature (to keep tally of total numbers into each, and when)(for the TRAINING phase)
   val learning =  mutable.Map.empty[
@@ -84,8 +82,9 @@ class Perceptron(n_classes:Int) {
     classnum_vector.zipWithIndex.maxBy(_._1)._2   // in vector order (stabilizes) ///NOT : (and alphabetically too)
   }
 
-  def average(w : WeightLearner):Float = (w.current*(seen-w.ts) + w.total) / seen // This is dynamically calculated
   def current(w : WeightLearner):Float =  w.current
+  def average(w : WeightLearner):Float = (w.current*(seen-w.ts) + w.total) / seen // This is dynamically calculated
+  // No need for average_weights() function - it's all done dynamically
   
   def score(features: Map[Feature, Score], score_method: WeightLearner => Float): ClassVector = { // Return 'dot-product' score for all classes
     if(false) { // This is the functional version : 3023ms for 1 train_all, and 0.57ms for a sentence
@@ -110,12 +109,10 @@ class Perceptron(n_classes:Int) {
         .filter( pair => pair._2 != 0 )  // if the 'score' multiplier is zero, skip
         .map{ case (Feature(name,data), score) => {  // Ok, so given a particular feature, and score to weight it by
           if(learning.contains(name) && learning(name).contains(data)) {
-            for {
-              (classnum, weight_learner) <- learning(name)(data)
-            } {
+            learning(name)(data).foreach { case (classnum, weight_learner) => {
               //println(s"classnum = ${classnum} n_classes=${n_classes}")
               scores(classnum) += score * score_method(weight_learner)
-            }
+            }}
           }
         }}
       scores.toVector
@@ -142,8 +139,6 @@ class Perceptron(n_classes:Int) {
     }
   }
   
-  // No need for average_weights - it's all done dynamically
-
  
   // http://stackoverflow.com/questions/23182577/even-trivial-serialization-examples-in-scala-dont-work-why
   // http://stackoverflow.com/questions/23072118/scala-pickling-how
@@ -182,11 +177,9 @@ object Tagger {  // Here, tag == Part-of-Speech
   
   // Make a tag dictionary for single-tag words : So that they can be 'resolved' immediately, as well as the class list
   def classes_and_tagdict(training_sentences: List[Sentence]): (Vector[ClassName], Map[Word, ClassNum])  = {
-    // First, get the set of classnames, and the counts for all the words and tags
-
-/*
-    // functional approach takes 120ms on full training data
-    val (class_set, full_map) =
+    
+    def functional_approach(): (Set[ClassName], Map[ Word, Map[ClassName, Int] ]) = {
+      // takes 120ms on full training data
       training_sentences.foldLeft( ( Set[ClassName](), Map[ Word, Map[ClassName, Int] ]() ) ) { case ( (classes, map), sentence ) => {
         sentence.foldLeft( (classes, map) ) { case ( (classes, map), word_data) => {
           val count = map.getOrElse(word_data.norm, Map[ClassName, Int]()).getOrElse(word_data.pos, 0:Int)
@@ -199,23 +192,30 @@ object Tagger {  // Here, tag == Part-of-Speech
           )
         }}
       }}
-*/
-
-    // mutable external collection approach takes 60ms on full training data !
-    val class_set = mutable.Set[ClassName]()
-    val full_map  = mutable.Map[ Word, mutable.Map[ClassName, Int] ]()
-                    //.withDefault( k => mutable.Map[ClassName, Int]().withDefaultValue(0) )       // FAIL - reuse
-                    //.withDefaultValue( new mutable.Map[ClassName, Int]().withDefaultValue(0) )   // FAIL - types
-                    
-    for {
-      sentence <- training_sentences
-      word_data <- sentence
-    } {
-      class_set += word_data.pos
-      full_map.getOrElseUpdate(word_data.norm, mutable.Map[ClassName, Int]().withDefaultValue(0))(word_data.pos) += 1
-      //println(s"making (${word_data.norm})(${word_data.pos})=>(${full_map(word_data.norm)(word_data.pos)})")
     }
-          
+    
+    def mutational_approach(): (mutable.Set[ClassName], mutable.Map[ Word, mutable.Map[ClassName, Int] ]) = {
+      // takes 60ms on full training data !
+      val class_set = mutable.Set[ClassName]()
+      val full_map  = mutable.Map[ Word, mutable.Map[ClassName, Int] ]()
+                      //.withDefault( k => mutable.Map[ClassName, Int]().withDefaultValue(0) )       // FAIL - reuse
+                      //.withDefaultValue( new mutable.Map[ClassName, Int]().withDefaultValue(0) )   // FAIL - types
+                      
+      for {
+        sentence <- training_sentences
+        word_data <- sentence
+      } {
+        class_set += word_data.pos
+        full_map.getOrElseUpdate(word_data.norm, mutable.Map[ClassName, Int]().withDefaultValue(0))(word_data.pos) += 1
+        //println(s"making (${word_data.norm})(${word_data.pos})=>(${full_map(word_data.norm)(word_data.pos)})")
+      }
+      
+      (class_set, full_map)
+    }
+    
+    // First, get the set of classnames, and the counts for all the words and tags
+    val (class_set, full_map) = if(true) functional_approach() else mutational_approach()
+    
     // Convert the set of classes into a nice map, with indexer
     val classes = class_set.toVector.sorted  // This is alphabetical
     val class_map = classes.zipWithIndex.toMap
