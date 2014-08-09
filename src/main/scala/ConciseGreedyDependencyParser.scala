8c8b9c690ae58781849854c13b70350a01629ef9
Martin Andrews <GitHub@mdda.net>
2014-08-09 17:56:29 +0800

    unigrams and pairs done

--- src/main/scala/ConciseGreedyDependencyParser.scala
+++ src/main/scala/ConciseGreedyDependencyParser.scala
@@ -503,7 +503,6 @@ class DependencyMaker(tagger:Tagger) {
     
     def extract_features(words:Vector[Word], tags:Vector[ClassName]):Map[Feature,Score] = {
       val feature_set = mutable.Set[Feature]() 
-      //feature_set += Feature("w suffix",   word(i).takeRight(3))  
       
       def get_stack_context[T<:String](data:Vector[T]):(T,T,T) = ( // Applies to both Word and ClassName (depth is implict from stack length)
         // NB: Always expecting 3 entries back...
@@ -541,53 +540,56 @@ class DependencyMaker(tagger:Tagger) {
       //   s0b1, s0b2: Two leftmost children of the top word of the stack
       //   s0f1, s0f2: Two rightmost children of the top word of the stack
 
-      val s0 = if(stack.isEmpty) -1 else stack.head
+      val stack0 = if(stack.isEmpty) -1 else stack.head
 
       val (ws0, ws1, ws2) = get_stack_context(words)
       val (ts0, ts1, ts2) = get_stack_context(tags)
 
+      val (wn0, wn1, wn2) = get_buffer_context(words)
+      val (tn0, tn1, tn2) = get_buffer_context(tags)
+    
+      val (vn0b, wn0b1, wn0b2) = get_parse_context(i, parse.lefts, words)
+      val (_   , tn0b1, tn0b2) = get_parse_context(i, parse.lefts, tags)
 
+      val (vn0f, wn0f1, wn0f2) = get_parse_context(i, parse.rights, words)
+      val (_,    tn0f1, tn0f2) = get_parse_context(i, parse.rights, tags)
+      
+      val (vs0b, ws0b1, ws0b2) = get_parse_context(stack0, parse.lefts, words)
+      val (_,    ts0b1, ts0b2) = get_parse_context(stack0, parse.lefts, tags)
+    
+      val (vs0f, ws0f1, ws0f2) = get_parse_context(stack0, parse.rights, words)
+      val (_,    ts0f1, ts0f2) = get_parse_context(stack0, parse.rights, tags)
+      
+          
+      //  String-distance :: Cap numeric features at 5? 
+      val ds0n0 = if(stack0 >= 0) math.min(i - stack0, 5) else 0
+      
+      feature_set += Feature("bias",       "")  // It's useful to have a constant feature, which acts sort of like a prior
+
+      //feature_set += Feature("w suffix",   word(i).takeRight(3))  
+
+      // Add word and tag unigrams
+      List(wn0, wn1, wn2, ws0, ws1, ws2, wn0b1, wn0b2, ws0b1, ws0b2, ws0f1, ws0f2).foreach(
+        word => { if(word!=0) feature_set += Feature("w", word) }
+      )
+      List(tn0, tn1, tn2, ts0, ts1, ts2, tn0b1, tn0b2, ts0b1, ts0b2, ts0f1, ts0f2).foreach(
+        tag  => { if(tag!=0) feature_set += Feature("t", tag) }
+      )
+      
+      // Add word/tag pairs
+      List((wn0, tn0), (wn1, tn1), (wn2, tn2), (ws0, ts0)).zipWithIndex.foreach{ 
+        case ((word,tag), idx) => { if( word!=0 || tag!=0 ) feature_set += Feature(s"wt$idx", s"w=$word t=$tag") }
+      }
+
+/*      
+*/
+
+    
       // All weights on this set of features are ==1
       feature_set.map( f => (f, 1:Score) ).toMap
     }
 /*    
 def extract_features(words, tags, n0, n, stack, parse):
-
-    Ws0, Ws1, Ws2 = get_stack_context(depth, stack, words)
-    Ts0, Ts1, Ts2 = get_stack_context(depth, stack, tags)
-   
-    Wn0, Wn1, Wn2 = get_buffer_context(n0, n, words)
-    Tn0, Tn1, Tn2 = get_buffer_context(n0, n, tags)
-    
-    Vn0b, Wn0b1, Wn0b2 = get_parse_context(n0, parse.lefts, words)
-    Vn0b, Tn0b1, Tn0b2 = get_parse_context(n0, parse.lefts, tags)
-    
-    Vn0f, Wn0f1, Wn0f2 = get_parse_context(n0, parse.rights, words)
-    _, Tn0f1, Tn0f2 = get_parse_context(n0, parse.rights, tags)
-  
-    Vs0b, Ws0b1, Ws0b2 = get_parse_context(s0, parse.lefts, words)
-    _, Ts0b1, Ts0b2 = get_parse_context(s0, parse.lefts, tags)
-
-    Vs0f, Ws0f1, Ws0f2 = get_parse_context(s0, parse.rights, words)
-    _, Ts0f1, Ts0f2 = get_parse_context(s0, parse.rights, tags)
-    
-    # Cap numeric features at 5? 
-    # String-distance
-    Ds0n0 = min((n0 - s0, 5)) if s0 != 0 else 0
-
-    features['bias'] = 1
-    # Add word and tag unigrams
-    for w in (Wn0, Wn1, Wn2, Ws0, Ws1, Ws2, Wn0b1, Wn0b2, Ws0b1, Ws0b2, Ws0f1, Ws0f2):
-        if w:
-            features['w=%s' % w] = 1
-    for t in (Tn0, Tn1, Tn2, Ts0, Ts1, Ts2, Tn0b1, Tn0b2, Ts0b1, Ts0b2, Ts0f1, Ts0f2):
-        if t:
-            features['t=%s' % t] = 1
-
-    # Add word/tag pairs
-    for i, (w, t) in enumerate(((Wn0, Tn0), (Wn1, Tn1), (Wn2, Tn2), (Ws0, Ts0))):
-        if w or t:
-            features['%d w=%s, t=%s' % (i, w, t)] = 1
 
     # Add some bigrams
     features['s0w=%s,  n0w=%s' % (Ws0, Wn0)] = 1
