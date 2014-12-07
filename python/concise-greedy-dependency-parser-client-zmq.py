bbe66a7c89207f5ae273767b870979c34482c079
Martin Andrews <GitHub@mdda.net>
2014-12-07 23:04:38 +0800

    Added CGDP process killer

--- python/concise-greedy-dependency-parser-client-zmq.py
+++ python/concise-greedy-dependency-parser-client-zmq.py
@@ -1,8 +1,9 @@
 #
 # Request-reply client in Python
-# Connects REQ socket to tcp://localhost:5560
-# Sends "Hello" to server, expects "World" back
+# Connects REQ socket to tcp://localhost:5559 (expecting a broker's bind() there)
 #
+
+import json
 
 # Tried : "yum install zeromq-devel"    ## Happens to be 2.2 on Fedora 20
 # Using : "yum install zeromq3-devel"   ## Happens to be 3.2.4 on Fedora 20
@@ -16,11 +17,35 @@ socket.connect("tcp://localhost:5559")
 
 print("Client is connected to socket on broker frontend")
 
+if False : # This is the Hello World test
+  # Do 10 requests, waiting each time for a response
+  for request in range(1,11):
+      print("Sending %d" % request)
+      socket.send(b"Hello")
+      
+      # Wait for response
+      message = socket.recv()
+      print("Received reply %d [%s]" % (request, message))
+
 # Do 10 requests, waiting each time for a response
-for request in range(1,11):
-    print("Sending %d" % request)
-    socket.send(b"Hello")
+for num in range(1,11):
+    body = {
+      'sentences': [
+        'Between 1994 and 2000, Ms Ding worked in the Shenzhen office of Yixing Silk-Linen Factory and was in charge of regional sales. ', 
+      ],
+    }
+    #r = requests.post("http://127.0.0.1:5001/redcatlabs/handshakes/api/v1.0/parse", data=json.dumps(body))
+
+    # See : http://augustl.com/blog/2013/zeromq_instead_of_http/
+    request = dict(
+      method = 'POST',
+      path = 'redcatlabs/handshakes/api/v1.0/parse',
+      body = body,
+    )
+    
+    print("Sending Request # %d" % num)
+    socket.send(json.dumps(request))
     
     # Wait for response
     message = socket.recv()
-    print("Received reply %s [%s]" % (request, message))
+    print("Received reply # %d [%s]" % (num, message))
